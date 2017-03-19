036e24e7aec76d489698dfc7acb90caebcccd9ff
t2t2 <taavot@gmail.com>
2017-03-19 19:50:35 +0200

    Bug when no image

--- site/templates/partials/episode-card.php
+++ site/templates/partials/episode-card.php
@@ -1,15 +1,17 @@
 <?php
 $image = $page->image ?: $page->parent->image;
-$thumb = $image->size(320, 180);
+$thumb = $image ? $image->size(320, 180) : null;
 ?>
 <div class="<?= isset($classes) ? $classes . ' ' : '' ?>card">
-	<div class="card-image">
-		<a href="<?= $page->url ?>">
-			<figure class="image is-16by9">
-				<img src="<?= $thumb->url; ?>" alt="<?= $page->title ?>" />
-			</figure>
-		</a>
-	</div>
+	<?php if ($thumb): ?>
+		<div class="card-image">
+			<a href="<?= $page->url ?>">
+				<figure class="image is-16by9">
+					<img src="<?= $thumb->url; ?>" alt="<?= $page->title ?>" />
+				</figure>
+			</a>
+		</div>
+	<?php endif; ?>
 	<div class="card-content">
 		<h3 class="title is-4">
 			<a href="<?= $page->url ?>">
